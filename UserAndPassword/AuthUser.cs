0f5434c52b41a59dbb6684df6aba188f7dd3788b
Roy Lovitt <lovittl@my.erau.edu>
2018-03-04 11:26:51 -0500

    update psudocode in #7

--- UserAndPassword/AuthUser.cs
+++ UserAndPassword/AuthUser.cs
@@ -8,9 +8,13 @@ namespace UserAndPassword
 {
     class AuthUser
     {
-        // When allow user selects option 2 allow them to enter a user name and password
-        // check the user name and password against user names and passwords entered in option 1 and 
-        // if user name and password are found in data structure in UserPass then return feedback to the user
+        /*
+        When user selects option 2 allow them to enter a user name and password
+           check the user name and password against user names and passwords entered in option 1 and 
+        
+        if user name and password are found in data structure in class UserPass (or not) then return feedback to the user
+                
         // STRETCH GOAL:  Simulate decryption of password 
+        */
     }
 }
